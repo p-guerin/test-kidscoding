cc456e410f20a5da1fc40619dec5bbec9598186e
p-guerin <guerin.pierre26@gmail.com>
2015-07-17 23:18:07 +0200

    Routes syntaxe modification de ExpressJS 3 a ExpressJS 4 et route de la page inscription dans app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var server_port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+var router = express.Router();
+
+router.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));  
+});
+router.get('/inscription', function(req, res) {
+    res.sendFile(path.join(__dirname + '/inscription.html'));
 });
 
-app.listen(3000);
+
+app.use('/', router);
+app.use('/inscription', router);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(server_port);
+console.log('Start node server on http://localhost:' + server_port);
